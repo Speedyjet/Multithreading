415f5a1b8d8e49adca320a592217300a4bc48110
Oleh Brusentsov <Oleh_Brusentsov@epam.com>
2022-06-10 11:55:41 +0300

    First task completed but need to refactor

--- MultiThreading.Task1.100Tasks/Program.cs
+++ MultiThreading.Task1.100Tasks/Program.cs
@@ -4,6 +4,8 @@
  * “Task #0 – {iteration number}”.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task1._100Tasks
 {
@@ -11,9 +13,30 @@ namespace MultiThreading.Task1._100Tasks
     {
         const int TaskAmount = 100;
         const int MaxIterationsCount = 1000;
+        private static readonly List<Task> Tasks = new List<Task>(100);
 
         static void Main(string[] args)
         {
+            for (int i = 0; i < TaskAmount; i++)
+            {
+                var task = new Task(() =>
+                {
+                    for (int j = 0; j < MaxIterationsCount; j++)
+                    {
+                        var taskId = Task.CurrentId;
+                        Output(taskId, j);
+                    }
+                });
+                
+                Tasks.Add(task);
+            }
+
+            foreach (var task in Tasks)
+            {
+                task.Start();
+            }
+
+            
             Console.WriteLine(".Net Mentoring Program. Multi threading V1.");
             Console.WriteLine("1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.");
             Console.WriteLine("Each Task should iterate from 1 to 1000 and print into the console the following string:");
@@ -21,16 +44,17 @@ namespace MultiThreading.Task1._100Tasks
             Console.WriteLine();
             
             HundredTasks();
-
+            Task.WaitAll();
             Console.ReadLine();
         }
 
-        static void HundredTasks()
+        private static void HundredTasks()
         {
-            // feel free to add your code here
+
         }
 
-        static void Output(int taskNumber, int iterationNumber)
+
+        static void Output(int? taskNumber, int iterationNumber)
         {
             Console.WriteLine($"Task #{taskNumber} – {iterationNumber}");
         }
