57f0bc00602a60a4066c251629873a0f154000ec
Oleh Brusentsov <Oleh_Brusentsov@epam.com>
2022-06-12 17:48:16 +0300

    second task completed

--- MultiThreading.Task2.Chaining/Program.cs
+++ MultiThreading.Task2.Chaining/Program.cs
@@ -6,11 +6,14 @@
  * Fourth Task – calculates the average value. All this tasks should print the values to console.
  */
 using System;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task2.Chaining
 {
     class Program
     {
+        private static Random Rnd = new Random();
+
         static void Main(string[] args)
         {
             Console.WriteLine(".Net Mentoring Program. MultiThreading V1 ");
@@ -21,9 +24,51 @@ namespace MultiThreading.Task2.Chaining
             Console.WriteLine("Fourth Task – calculates the average value. All this tasks should print the values to console");
             Console.WriteLine();
 
-            // feel free to add your code
+            var firstTask = Task.Factory.StartNew(() => 
+            {
+                var ints = new int[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    int randomInt = Rnd.Next(1, 10);
+                        Console.WriteLine("random int is {0} ", randomInt);
+                        ints[i] = randomInt;
+                        Console.WriteLine(String.Concat(ints));
+                }
+                return ints;
+            });
+            Task<int[]> secondTask = firstTask.ContinueWith((prevTask) =>
+            {
+                var intArray = prevTask.Result;
+                   for (int i = 0; i < intArray.Length; i++)
+                    {
 
-            Console.ReadLine();
+                        var multiplier = Rnd.Next(1, 10);
+                        Console.WriteLine("multipliier is {0}", multiplier);
+                        Console.WriteLine("before {0}", intArray[i]);
+                        intArray[i] = intArray[i] * multiplier;
+                        Console.WriteLine("after {0}", intArray[i]);
+                    }
+                Console.WriteLine(String.Concat(intArray));
+                return intArray;
+            });
+            var thirdTask = secondTask.ContinueWith((result) =>
+            {
+                int[] unsortedArray = result.Result;
+                Array.Sort(unsortedArray);
+                Console.WriteLine(String.Concat(unsortedArray));
+                return unsortedArray;
+            });
+            var fourthTask = thirdTask.ContinueWith((result) =>
+            {
+                var sum = 0;
+                foreach (var item in result.Result)
+                {
+                    sum += item;
+                }
+            Console.WriteLine("Average is {0}", sum / result.Result.Length);
+            });
+            Task.WaitAll();
+            Console.ReadKey();
         }
     }
 }
