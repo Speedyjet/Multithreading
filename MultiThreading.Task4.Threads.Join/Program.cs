28f204629c55cd87ff32e264a7eaec3133a787b7
Oleh Brusentsov <Oleh_Brusentsov@epam.com>
2022-06-19 20:50:28 +0300

    4th task completed

--- MultiThreading.Task4.Threads.Join/Program.cs
+++ MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,11 +10,15 @@
  */
 
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task4.Threads.Join
 {
     class Program
     {
+        private static Semaphore _semaphore = new Semaphore(1, 1);
+        private static int _counter = 10;
         static void Main(string[] args)
         {
             Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
@@ -26,9 +30,36 @@ namespace MultiThreading.Task4.Threads.Join
 
             Console.WriteLine();
 
-            // feel free to add your code
+            //TypeA(_counter);
+            TypeB(_counter);
 
             Console.ReadLine();
+        }
+
+        private static void TypeB(object counter)
+        {
+            if ((int)counter > 0)
+            {
+                _semaphore.WaitOne();
+                Console.WriteLine("obj = {0}", counter);
+                int innerCounter = (int)counter;
+                innerCounter--;
+                _semaphore.Release();
+                ThreadPool.QueueUserWorkItem(new WaitCallback(TypeB), innerCounter);
+            }
+        }
+
+        private static void TypeA(object obj)
+        {
+            {
+                if ((int) obj > 0)
+                {
+                    Console.WriteLine("obj = {0}", obj);
+                    var innerThread = new Thread(TypeA);
+                    innerThread.Start((int)obj - 1);
+                    innerThread.Join();
+                }
+            }
         }
     }
 }
